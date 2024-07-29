c9bb7a838144296a7b3d3f0d272dc2b1efa12c14
Mohamed Ridha <hamodeygame45@gmail.com>
2024-07-29 10:50:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ API_HASH = getenv("API_HASH","f1db2e38b2c73448ef09c504187e888d")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Hamodey2006:Hamodey2006@cluster0.xilnp5n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
-OWNER_ID = int(getenv("OWNER_ID", 5488301592))
+OWNER_ID = int(getenv("OWNER_ID", 815010872))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/YY7365923851:AAGlgzoywP0I1imV97WN7eEdkCt6_imWi_E5Y8")

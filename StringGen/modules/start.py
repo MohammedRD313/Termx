79df9fe22072be741db95c7730bbce52dd82344b
Mohamed Ridha <hamodeygame45@gmail.com>
2024-07-29 14:12:22 +0300

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -8,13 +8,13 @@ from StringGen.utils import add_served_user, keyboard
 async def f_start(_, message: Message):
     await message.reply_text(
         text=f"""
-مرحبا بك {message.from_user.first_name} فـي بـوت اسـتـخـراج جلسات العقرب
+مرحبا بك فـي بـوت اسـتـخـراج جلسات العقرب | {message.from_user.first_name} 
 
 ✎┊‌ يمكنك استـخـراج التالـي 
-
 ✎┊‌ تـلـيـثـون 
 ✎┊‌ بايـروجـرام 
 
+✎┊‌ تم انشاء البوت بواسطة: @Zo_r0 , @I_e_e_l
 """,
         reply_markup=keyboard,
         disable_web_page_preview=True,

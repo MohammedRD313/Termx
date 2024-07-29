96450b823cad1c6e8b52b4780ee864126882cde7
Mohamed Ridha <hamodeygame45@gmail.com>
2024-07-29 14:50:03 +0300

    Update start.py

--- StringGen/modules/start.py
+++ StringGen/modules/start.py
@@ -7,14 +7,18 @@ from StringGen.utils import add_served_user, keyboard
 @Anony.on_message(filters.command("start") & filters.private & filters.incoming)
 async def f_start(_, message: Message):
     await message.reply_text(
-        text=f"""
-مرحبا بك فـي بـوت اسـتـخـراج جلسات العقرب | {message.from_user.first_name} 
+        text=f"""</b>
+{message.from_user.first_name} | مرحبا بك فـي بـوت اسـتـخـراج جلسات العقرب  
 
 ✎┊‌ يمكنك استـخـراج التالـي 
 ✎┊‌ تـلـيـثـون 
 ✎┊‌ بايـروجـرام 
 
-✎┊‌ تم انشاء البوت بواسطة: @Zo_r0 , @I_e_e_l
+- لبدء الاستخراج اضغط Start bot
+- لمعرفة كيفية الاستخراج اضغط Order bot
+
+✎┊‌ تم انشاء البوت بواسطة: @Zo_r0 | @I_e_e_l
+</b>
 """,
         reply_markup=keyboard,
         disable_web_page_preview=True,
